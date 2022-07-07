1213686ee81a3ff1925eb7099f35f1a622c53110
Barak <zorefbarak@gmail.com>
2022-07-07 13:15:18 +0300

    cancel the order option when there are no cart items

--- client/src/app/components/cart/cart.component.ts
+++ client/src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import ICartItem from 'src/app/models/icart-item.model';
 import ICart from 'src/app/models/icart.model';
 import { CartItemsService } from 'src/app/services/cart-items.service';
@@ -18,6 +19,7 @@ export class CartComponent implements OnInit {
   constructor(
     public _cartItemsService: CartItemsService,
     public _cartService: CartService,
+    public _messageService: MessageService,
     public router: Router
   ) { }
 
@@ -41,6 +43,15 @@ export class CartComponent implements OnInit {
   editItemQuantity(cartItem): void{
     this.currentCartItem = cartItem;
     this.displayModal = true;
+  }
+
+  onOrderButtonClicked(): void{
+    if(this._cartItemsService.cartItems.length){
+      this.router.navigate(['/order']);
+    }
+    else{
+      this._messageService.add({ key: 'appToast', severity: 'error', summary: 'Error', detail: 'There are no cart items in the cart' });
+    }
   }
 
 }
