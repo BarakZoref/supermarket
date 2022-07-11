999c061e8af5fa43f9c725576bfa0a8790f4c4b8
Barak <zorefbarak@gmail.com>
2022-07-11 15:21:19 +0300

    indentation fixed and swagger downloaded

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -5,49 +5,49 @@ const router = express.Router();
 
 // ADD USER
 // POST http://localhost:4200/users
-    router.post("/", async (request, response) => {
+router.post("/", async (request, response) => {
 
-        let userRegistrationData = request.body;
-        userRegistrationData.role = "user";
-        try {
-            await usersLogic.addUser(userRegistrationData);
-            response.json({err: false, msg: "user was added successfuly!"});
-        }
-        catch (e) {
-            console.error(e);
-            response.status(600).send(e.message);        
-        }
-    });
+    let userRegistrationData = request.body;
+    userRegistrationData.role = "user";
+    try {
+        await usersLogic.addUser(userRegistrationData);
+        response.json({err: false, msg: "user was added successfuly!"});
+    }
+    catch (e) {
+        console.error(e);
+        response.status(600).send(e.message);        
+    }
+});
 
-    // LOG IN
-    // POST http://localhost:4200/users/login
-    router.post("/login", async (request, response) => { 
-        let userLogInData = request.body;
-        try{
-           let successfulLoginResponse = await usersLogic.logIn(userLogInData);
-    
-            response.json(successfulLoginResponse);
-        }
-        catch(e){
-            console.error(e);
-            response.status(600).send(e.message);
-        }
-    });
+// LOG IN
+// POST http://localhost:4200/users/login
+router.post("/login", async (request, response) => { 
+    let userLogInData = request.body;
+    try{
+        let successfulLoginResponse = await usersLogic.logIn(userLogInData);
 
-    // IS USER EXIST
-    // POST http://localhost:4200/users/is_exist
-    router.post("/is_exist", async (request, response) => { 
-        let userData = request.body;
-        try{
-           let isExists = await usersLogic.isUserExist(userData);
-    
-            response.json(isExists);
-        }
-        catch(e){
-            console.error(e);
-            response.status(600).send(e.message);
-        }
-    });
+        response.json(successfulLoginResponse);
+    }
+    catch(e){
+        console.error(e);
+        response.status(600).send(e.message);
+    }
+});
+
+// IS USER EXIST
+// POST http://localhost:4200/users/is_exist
+router.post("/is_exist", async (request, response) => { 
+    let userData = request.body;
+    try{
+        let isExists = await usersLogic.isUserExist(userData);
+
+        response.json(isExists);
+    }
+    catch(e){
+        console.error(e);
+        response.status(600).send(e.message);
+    }
+});
 
 
 
