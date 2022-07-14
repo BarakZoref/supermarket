855f58450784cb64a144d990a2e7c4d50f816344
Barak <zorefbarak@gmail.com>
2022-07-14 14:04:32 +0300

    more swagger functions was added

--- server/swagger.js
+++ server/swagger.js
@@ -7,7 +7,7 @@ fs.readdirSync('./controllers/').forEach(file => {
 	endpointFilesArray.push(`./controllers/${file}`);
 });
 
-const outputFile = './swagger_output.json';
+const outputFile = './swagger1_output.json';
 (async () => {
 	await swaggerAutogen(outputFile, endpointFilesArray);
 	require('./index');
